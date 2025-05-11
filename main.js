9e6a7f321a06dff01de62f83c97802b01d577a93
TalalAlrashedi <TalalAlrashedi24@hotmail.com>
2025-05-11 11:26:07 +0300

    finish

--- main.js
+++ main.js
@@ -5,38 +5,21 @@ fetch("https://rickandmortyapi.com/api/character")
   .then((data) => {
     console.log(data);
     data.results.map((element) => {
-        let result = element.image
-        console.log(result);
+      let card = document.createElement("div");
+      card.classList.add("card");
 
-        let char = document.createElement("h3")
-        
-        let image = document.createElement("img")
-        image.src = element.image
-        char.innerText = element.name
+      let cardDetails = document.createElement("div");
+      cardDetails.classList.add("card-details");
 
-        container.appendChild(char)
-        container.appendChild(image)
+      let char = document.createElement("h3");
 
+      let image = document.createElement("img");
+      image.src = element.image;
+      char.innerText = element.name;
+
+      cardDetails.appendChild(char);
+      cardDetails.appendChild(image);
+      card.appendChild(cardDetails);
+      container.appendChild(card);
     });
-
   });
-
-// let container = document.getElementById("container");
-
-// fetch("https://rickandmortyapi.com/api/character")
-//   .then((response) => response.json())
-//   .then((data) => {
-//     console.log(data);
-//     data.results.map((element) => {
-//       let result = element.image;
-//       console.log(result);
-
-//       let char = document.createElement("h3");
-//       let image = document.createElement("img");
-//       image.src = element.image;
-//       char.innerText = element.name;
-
-//       container.appendChild(char);
-//       container.appendChild(image);
-//     });
-//   });
